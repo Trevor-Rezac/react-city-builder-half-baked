3467b9106bf1f1b6501bda1faff04998d8bc1b07
TrevorRezac <trev.rezac@gmail.com>
2022-01-31 15:57:55 -0800

    images render on dropdown change

--- src/City.js
+++ src/City.js
@@ -7,6 +7,8 @@ export default function City({ skylineId, waterfrontId, castleId }) {
       {/* this component should take 3 props: waterfrontId, skylineId, and castlId */}
       {/* use these 3 ids in img tags load appropriate images that live in the public directory */}
       <img className='skyline-img' src={`skyline-${skylineId}.jpeg`}></img>
+      <img className='waterfront-img' src={`waterfront-${waterfrontId}.jpeg`}></img>
+      <img className='castle-img' src={`castle-${castleId}.jpeg`}></img>
     </div>
   );
 }
